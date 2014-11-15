7ffc1ee114780ad7cff335232b1a4e0fad0fc3c9
Chris <x.sinclair@gmail.com>
2014-11-15 19:59:15 +0000

    shane's commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,78 @@
-var patient = new Firebase("https://dhoxstamp.firebaseio.com/");
+var CurrentPage,
+Latitude = 0,
+Longitude = 0,
+Age, Gender, Weight, Height;
+patient = new Firebase("https://dhoxstamp.firebaseio.com/");
+
 var StampApp = {
   Init: function () {
-    //check if theres records that haven't been pushed
-    // if so push
-
-    $(".next").on("click",function(){
-      var next_page = $(this).attr("next_page");
-      StampApp.Page.Switch(next_page);
+    $(".btn[data-attr-next]").on("click",function(){
+      var next = $(this).attr("data-attr-next");
+      if(next == "complete"){
+        StampApp.Store.Complete();
+      }else{
+        StampApp.Page.Switch(next);
+      }
     });
   },
-  Storage : function(){
-    CurrentPage = 0;
-    Latitude = 0;
-    Longitude = 0;
-  },
   Page: {
-    Load:{
-      
+    Check:function(page){
+      switch (page) {
+        case 1:
+          if($("input[name=year]").val() === "" && $("input[name=month]").val() === ""){
+            StampApp.Error("You must say what age the baby is.");
+            return false;
+          }
+            Age = StampApp.Store.Age($("input[name=year]").val(), $("input[name=month]").val());
+          break;
+        case 2:
+          if(!$("input[name=female]").prop("checked") && !$("input[name=male]").prop("checked")){
+            StampApp.Error("You must select what gender the baby is.");
+            return false;
+          }
+          if($("input[name=female]").prop("checked")){
+            Gender = StampApp.Store.Gender("F");
+          }else{
+            Gender = StampApp.Store.Gender("M");
+          }
+          break;
+        case 3:
+          if($("input[name=height]").val() === ""){
+            StampApp.Error("Height is required to continue.");
+            return false;
+          }
+          Height = $("input[name=height]").val();
+          break;
+        case 4:
+          if($("input[name=weight]").val() === ""){
+            StampApp.Error("You must enter the weight of the baby.");
+            return false;
+          }
+          Weight = $("input[name=weight]").val();
+          break;
+      }
     },
     Switch:function(next){
-      StampApp.Storage.CurrentPage = next;
+      StampApp.Page.Check(next - 1);
+      CurrentPage = next;
       $(".step").slideUp("fast",function(){
-        $(".step[number="+StampApp.Storage.CurrentPage+"]").slideDown("fast");
+        $(".step[data-attr-num="+CurrentPage+"]").slideDown("fast");
       });
     }
   },
   Store: {
-    Init: function(){
-      patient.child("records");
-      StampApp.GeoLocation.Get();
-    },
-    Age: function(dob){
+    Age: function(year, month){
       //format date
-      // months
-      // integer
-      return patient.set({"dob":dob});
+      if(month === ""){
+        month = 0;
+      }
+      if(year === ""){
+        year = 0;
+      }
+      age = (year * 12) + month;
+      return age;
     },
     Gender: function(gender){
-      // format gender
-      // M/F/O
       return gender;
     },
     Weight: function(weight){
@@ -49,38 +83,44 @@ var StampApp = {
       // mm
       return height;
     },
-    Complete:function(age, gender, weight){
-      patient.push(
+    Complete:function(){
+      patient.child("records").push(
         {
-          "age": StampApp.Store.Age(age),
-          "gender": StampApp.Store.Gender(gender),
-          "weight":StampApp.Store.weight(weight),
-          "height":StampApp.Store.Height(height),
-          "longitude":StampApp.Storage.Longitude,
-          "latitude":StampApp.Storage.Latitude,
-        }
+          "age": Age,
+          "gender": Gender,
+          "weight":Weight,
+          "height":Height,
+          "longitude":Longitude,
+          "latitude":Latitude,
+        },
+        console.log("push")
       );
+    }
+  },
+  Error: {
+    Message: function(errorMessage){
+      $(".error p.message").append(errorMessage);
     }
   },
   Calculation:{
     Init:function(){
 
     }
-  },
-  GeoLocation:{
-    Get: function(){
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(ShowPosition);
-      }
-    },
-    ShowPosition: function(position){
-      StampApp.Storage.Latitude = position.coords.latitude;
-      StampApp.Storage.Longitude = position.coords.longitude;
-    }
   }
 };
 
 
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
+function showPosition(position) {
+    Latitude = position.coords.latitude;
+    Longitude = position.coords.longitude; 
+}
+
 $(function () {
+  getLocation();
   StampApp.Init();
 });
