9feab37561ffde59e7c093f03ab804380af0b7e4
Warren Elder <warrenelder84@gmail.com>
2014-11-16 10:33:49 +0000

    get flash data on php script

---
+++ flash_demo/send_data.php
@@ -0,0 +1,30 @@
+<!doctype html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Send Data</title>
+  </head>
+  <body>
+      <p>Hello</p>
+  
+  <script type="text/javascript" src="js/lib/jquery-2.1.1.min.js"></script>
+  <script type="text/javascript" src="js/lib/firebase-2.03.js"></script>
+  <script type="text/javascript" src="js/parameter.js"></script>
+  </body>
+</html>
+
+<?php 
+  $age              = $_GET["age"];
+  $gender           = $_GET["gender"];
+  $weight           = $_GET["weight"];
+  $height           = $_GET["height"];
+  $muac             = $_GET["muac"];
+  $recorded_on      = $_GET["recorded_on"];
+  $recorded_by      = $_GET["recorded_by"];
+  $location         = $_GET["location"];
+  $lat              = $_GET["lat"];
+  $long             = $_GET["long"];
+  $mother_weight    = $_GET["mother_weight"];
+  $risk             = $_GET["risk"];
+  $mobile           = $_GET["mobile"];
+?>
